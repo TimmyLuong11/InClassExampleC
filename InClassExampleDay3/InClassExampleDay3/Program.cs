2e9576e45d99471dabdebc325c555a6e0420a9be
Timothy Luong <Timothy.Luong@ou.edu>
2021-02-02 18:35:26 -0600

    Added variables, gather user input, output user input

--- InClassExampleDay3/InClassExampleDay3/Program.cs
+++ InClassExampleDay3/InClassExampleDay3/Program.cs
@@ -6,7 +6,29 @@ namespace InClassExampleDay3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declearing Variables
+            String firstName, lastName;
+            double gpa;
+
+            //Output to get user information
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaString);
+
+            gpa += 0.5; //gpa = gpa + 0.5 
+
+            //Output user information
+            string output = "Hello " + firstName + " " + lastName + "! " + "we bosted your gpa to " + gpa.ToString("N3");
+            Console.WriteLine(output);
+
+            output = $"Hello { firstName} { lastName}! we bosted your gpa to {gpa.ToString("N3")}";
+            Console.WriteLine(output);
         }
     }
 }
